e132a34db6f77d25f42dbecad8c8a8076d2aeee5
xXHachimanXx <globaldep90@gmail.com>
2021-09-17 09:32:02 -0300

    Finalizing frontend

---
+++ client-room/src/app/create-room/create-room.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Room } from '../room';
+import { RoomService } from '../room.service';
+
+@Component({
+  selector: 'app-create-room',
+  templateUrl: './create-room.component.html',
+  styleUrls: ['./create-room.component.css']
+})
+export class CreateRoomComponent implements OnInit {
+  room: Room = new Room();
+  submitted = false;
+
+  constructor(
+    private roomService: RoomService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void { }
+
+  newRoom(): void {
+    this.submitted = false;
+    this.room = new Room();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();    
+  }
+
+  gotoList() {
+    this.router.navigate(['/rooms']);
+  }
+
+  save() {
+    this.roomService.createRoom(this.room)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.room = new Room();
+    this.gotoList();
+  }
+
+}
